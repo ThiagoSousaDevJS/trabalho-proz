6b91b907af1cd1e701cd986b99cdb1b6b3573529
thiago Sousa <tiagosousa1968@gmail.com>
2025-03-23 19:10:38 -0300

    Atualiza settings.py e adiciona requirements.txt

--- appEmpresa/appEmpresa/settings.py
+++ appEmpresa/appEmpresa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-phzi-!apvn@kum2555$_s-adh*njghc4jon$%e98@&h25s4b)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
